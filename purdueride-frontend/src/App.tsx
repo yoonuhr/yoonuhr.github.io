a1d6f9e7f1c0a54c2b6e88e773067fb5b715ea8c
Yoon <yoonuhr@gmail.com>
2025-07-22 18:27:19 -0400

    change app css to app tsx

--- purdueride-frontend/src/App.tsx
+++ purdueride-frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import './App.css';
 
 function App() {
   return (
